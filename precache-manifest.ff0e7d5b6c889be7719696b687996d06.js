3501552eddfaec30a176fc66980d1131c9e642f1
Emily Bossiere <emilybossiere@gmail.com>
2019-07-08 17:45:25 -0700

    Updates

---
+++ precache-manifest.ff0e7d5b6c889be7719696b687996d06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b828f1f6a001f12e3442c6f7c2651a1a",
+    "url": "/emily-bossiere/index.html"
+  },
+  {
+    "revision": "f3790e16f701432d03f4",
+    "url": "/emily-bossiere/static/css/main.e2faf760.chunk.css"
+  },
+  {
+    "revision": "0720332867d970c1d1f6",
+    "url": "/emily-bossiere/static/js/2.8db7fec4.chunk.js"
+  },
+  {
+    "revision": "f3790e16f701432d03f4",
+    "url": "/emily-bossiere/static/js/main.605da617.chunk.js"
+  },
+  {
+    "revision": "486eaa3816531b7073aa",
+    "url": "/emily-bossiere/static/js/runtime~main.b304fb71.js"
+  },
+  {
+    "revision": "854f72d28fa23d81f7bf97fc416efe89",
+    "url": "/emily-bossiere/static/media/colorguess.854f72d2.png"
+  },
+  {
+    "revision": "6aa0e8db383d85746d2434d74698caf1",
+    "url": "/emily-bossiere/static/media/grabbag.6aa0e8db.png"
+  },
+  {
+    "revision": "0a09f6e5f4555f742dbfc15590ad6445",
+    "url": "/emily-bossiere/static/media/tictactoe.0a09f6e5.png"
+  }
+]);
